4afaf7c4716473ee7ebdb0243d55567930f5e434
songkyung <min.songkyung@gmail.com>
2020-12-30 13:46:31 +0900

    set githubSearchViewReactor

---
+++ GitHubSearch/GitHubSearch/GitHubSearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GitHubSearchViewController.swift
+//  GitHubSearch
+//
+//  Created by Running Raccoon on 2020/12/30.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+class GitHubSearchViewController: UIViewController, StoryboardView {
+    
+    
+    
+    
+    func bind(reactor: Reactor) {
+        <#code#>
+    }
+    
+}
